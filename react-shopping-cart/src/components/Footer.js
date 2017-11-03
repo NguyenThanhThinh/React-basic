9e4af1b943e9933d9760bdbad4782e56462c8655
thanhthinhcntt <thanhthinhcntt@gmail.com>
2017-11-04 02:32:50 +0700

    add header and footer

---
+++ react-shopping-cart/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class Footer extends Component {
+  render() {
+    return(
+      <div>
+        <hr />
+        <address>Can Tho</address>        
+      </div>
+    );
+  }
+}
+
+export default Footer;
