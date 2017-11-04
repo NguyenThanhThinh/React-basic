8699868ca8a4c807c98bb7054e3a9695566ef50e
thanhthinhcntt <thanhthinhcntt@gmail.com>
2017-11-04 09:57:24 +0700

    update file

--- react-shopping-cart/src/App.js
+++ react-shopping-cart/src/App.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ProductList from './components/ProductList';
+
 
 class App extends Component {
+ 
+}
   render() {
     return (
       <div className="App">
        <Header/>
+      
        <Footer/>
       </div>
     );
